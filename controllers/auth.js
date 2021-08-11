049aac6082cc51fc42e60bf6cf29106c41e5a18d
Axel <axosar@gmail.com>
2021-08-11 08:06:55 -0500

    Resgiter user to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,34 @@
-export const register = (req, res) => {
-  console.log(req.body);
-  res.json("register - response from controller");
+import User from '../models/user';
+import { hashPassword, comparePassword } from '../utils/auth';
+
+export const register = async (req, res) => {
+  try{
+    //console.log(req.body);
+    const { name, email, password } = req.body
+    //vaidation
+    if(!name) return res.status(400).send('Name is required')
+    if(!password || password.length < 6){
+      return res
+        .status(400)
+        .send("Password is required and should be 6 car long")
+    }
+    let userExist = await User.findOne({email}).exec();
+    if (userExist) return res.status(400).send('Email is taken')
+
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    }).save();
+
+    console.log('saved user', user);
+    return res.json({ok: true});
+
+  } catch(err){
+    console.log(err)
+    return res.status(400).send('Error. Try again');
+  }
 }
